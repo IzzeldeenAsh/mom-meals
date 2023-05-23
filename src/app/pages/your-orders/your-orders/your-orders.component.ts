3ae09897351a87189ab5e0b0bc2cc87cb4b60f0e
IzzeldeenAsh <ezraashour00@gmail.com>
2023-05-23 17:43:25 +0300

    commit

---
+++ src/app/pages/your-orders/your-orders/your-orders.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-your-orders',
+  templateUrl: './your-orders.component.html',
+  styleUrls: ['./your-orders.component.scss'],
+})
+export class YourOrdersComponent {}
